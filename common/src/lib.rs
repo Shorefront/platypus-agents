9435c1ecd5a93fe7f208e6cfb62ab209199dc56b
Ryan <rruckley@gmail.com>
2026-09-14 21:00:15 +1000

    feat: config

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,18 +1,19 @@
 use config::Config;
 use std::sync::OnceLock;
-use log::info;
+use log::{debug,info};
 use serde::Deserialize;
 
 pub fn get<'a, T: serde::Deserialize<'a>>(path: &str) -> Option<T> {
+    debug!("Config::get({path})");
     config().get::<T>(path).ok()
 }
 
-fn config() -> &'static Config {
+pub fn config() -> &'static Config {
     static CONFIG: OnceLock<Config> = OnceLock::new();
     CONFIG.get_or_init(|| {
         Config::builder()
             .add_source(
-                config::Environment::with_prefix("PLATYPUS")
+                config::Environment::with_prefix("platypus")
                     .try_parsing(true)
                     .separator("_")
                     .list_separator(","),
@@ -22,9 +23,10 @@ fn config() -> &'static Config {
     })
 }  
 
-pub fn register(_end_point : &str) {
+pub fn register(_end_point : &str, platypus : String) {
     // Registration logic here
     // USe reqwest to register end-point with Platypus
-    let platypus_server = get::<String>("SERVER").expect("Platypus server URL not configured");
-    log::info!("Platypus: {platypus_server}");
+
+
+    log::info!("Platypus: {platypus}");
 }
--- platypus-agent-dummy/src/main.rs
+++ platypus-agent-dummy/src/main.rs
@@ -1,12 +1,35 @@
 
 use common::register;
 use log::info;
+use config::{Config,Environment};
+use serde::Deserialize;
 
-fn main() {
+#[derive(Debug,Deserialize)]
+struct Settings {
+    pub platypus_host : String,
+    pub platypus_port : u16,
+    pub platypus_tls : bool,
+}
+
+fn main() -> Result<(),config::ConfigError> {
     let pkg = env!("CARGO_PKG_NAME");
     let ver = env!("CARGO_PKG_VERSION");
     env_logger::init();
     info!("Version\t: {} v{}", pkg, ver);
 
-    register("platypus.server");
+    let settings = Config::builder()
+        .set_default("port",8001)?
+        .set_default("tls",true)?
+        .add_source(
+            Environment::with_prefix("PLATYPUS")
+                .try_parsing(true)
+                .separator("_"),
+        )
+        .build()?;
+
+    let config : Settings = settings.try_deserialize()?;
+
+    register("platypus.server",config.platypus_host);
+
+    Ok(())
 }
